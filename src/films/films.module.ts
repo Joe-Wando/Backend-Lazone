4847a7bb2dbfeb088844be2f982701027ab2e948
Joe-Wando <jonathan6wando@gmail.com>
2026-07-19 17:12:14 +0200

    feat: ajout du service d'intégration TMDB

--- src/films/films.module.ts
+++ src/films/films.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { Film } from './entities/film.entity';
+import { TmdbService } from './tmdb.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Film])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Film]), HttpModule],
+  providers: [TmdbService],
+  exports: [TypeOrmModule, TmdbService],
 })
 export class FilmsModule {}
