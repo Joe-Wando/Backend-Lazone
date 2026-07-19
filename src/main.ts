55b2209af1b8cfc083d177b4c72a96d0651cac79
Joe-Wando <jonathan6wando@gmail.com>
2026-07-19 19:01:57 +0200

    docs: ajout de la documentation Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +15,18 @@ async function bootstrap() {
   );
 
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('La Zone — API')
+    .setDescription(
+      'API REST de la plateforme de réservation de tickets de cinéma La Zone',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
 }
