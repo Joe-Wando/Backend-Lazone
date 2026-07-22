afdfd02f3b4251678b5a65effda78b14667a55a8
Joe-Wando <jonathan6wando@gmail.com>
2026-07-22 04:26:03 +0200

    feat: ajout du controller salles avec routes protégées par rôle

--- src/salles/salles.module.ts
+++ src/salles/salles.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Salle } from './entities/salle.entity';
 import { SallesService } from './salles.service';
 import { CinemasModule } from '../cinemas/cinemas.module';
+import { SallesController } from './salles.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Salle]), CinemasModule],
+  controllers: [SallesController],
   providers: [SallesService],
   exports: [TypeOrmModule, SallesService],
 })
