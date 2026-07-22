120c1c42ac34ae05ed6eee4f035410c5f07783c1
Joe-Wando <jonathan6wando@gmail.com>
2026-07-22 04:20:06 +0200

    feat: ajout du service salles

--- src/salles/salles.module.ts
+++ src/salles/salles.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Salle } from './entities/salle.entity';
+import { SallesService } from './salles.service';
+import { CinemasModule } from '../cinemas/cinemas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Salle])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Salle]), CinemasModule],
+  providers: [SallesService],
+  exports: [TypeOrmModule, SallesService],
 })
 export class SallesModule {}
