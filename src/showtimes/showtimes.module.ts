69a212ec7546165d4e40b9cbc667f3aff3422812
Joe-Wando <jonathan6wando@gmail.com>
2026-07-22 05:07:25 +0200

    feat: ajout du controller showtimes avec routes protégées par rôle

--- src/showtimes/showtimes.module.ts
+++ src/showtimes/showtimes.module.ts
@@ -4,9 +4,11 @@ import { Showtime } from './entities/showtime.entity';
 import { ShowtimesService } from './showtimes.service';
 import { FilmsModule } from '../films/films.module';
 import { SallesModule } from '../salles/salles.module';
+import { ShowtimesController } from './showtimes.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Showtime]), FilmsModule, SallesModule],
+  controllers: [ShowtimesController],
   providers: [ShowtimesService],
   exports: [TypeOrmModule, ShowtimesService],
 })
