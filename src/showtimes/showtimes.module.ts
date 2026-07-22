92eb955f629ab9d02795fbe2a5a3bb35a0635262
Joe-Wando <jonathan6wando@gmail.com>
2026-07-22 04:51:46 +0200

    feat: ajout du service showtimes

--- src/showtimes/showtimes.module.ts
+++ src/showtimes/showtimes.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Showtime } from './entities/showtime.entity';
+import { ShowtimesService } from './showtimes.service';
+import { FilmsModule } from '../films/films.module';
+import { SallesModule } from '../salles/salles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Showtime])],
-  exports: [TypeOrmModule],
+  imports: [TypeOrmModule.forFeature([Showtime]), FilmsModule, SallesModule],
+  providers: [ShowtimesService],
+  exports: [TypeOrmModule, ShowtimesService],
 })
 export class ShowtimesModule {}
